3b372b0e73b122c9f8adf89ea4f85bce4330ae43
Jeff Ellenbogen <jeffellenbogen@gmail.com>
2019-07-21 17:57:49 -0600

    switched to passing pot and envelop
    also added 2nd function...
    circularDivision()

--- pyramidOfDestiny/pyramidOfDestiny.ino
+++ pyramidOfDestiny/pyramidOfDestiny.ino
@@ -7,16 +7,16 @@
 #define B   A1
 #define C   A2
 #define D   A3
+
 #define ENVELOPE_PIN A4
 #define POT_PIN A5
 #define BUTTON_PIN 8
 
+#define MAX_ENVELOPE 150
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, true);
 
-int potValue, envelopeValue, audioModifier, baseColor;
+int potValue, envelopeValue;
 int pattern = 1;
-int colorGradient = 40;
-int audioGradientMax = 500;
 
 
 void setup() {
@@ -28,22 +28,19 @@ void setup() {
 void loop() {  
   //Read envelopeValue and adjust audioModifier value based on that reading
   envelopeValue = analogRead(ENVELOPE_PIN);
-  envelopeValue = constrain(envelopeValue,0,100);
-  audioModifier = map(envelopeValue,6,100,0,audioGradientMax);
+  envelopeValue = constrain(envelopeValue,0,MAX_ENVELOPE);
 
   // Read potValue to set the baseColor for the pattern
   potValue = analogRead(POT_PIN);
-  baseColor = map(potValue,0,1023,0,1536);
-
 
   //Check for button Press to increment pattern
   buttonPressed();
   
   //select which pattern to display based on the current pattern
   if (pattern == 1)
-    drawColorRects();
+    drawColorRects(potValue, envelopeValue);
   else if (pattern == 2)
-    circularDivision();
+    circularDivision(potValue, envelopeValue);
   else if (pattern == 3)
     redBlueFade();
   else if (pattern == 4)
@@ -64,20 +61,98 @@ void buttonPressed()
       pattern = 1;  
 }
 
-void drawColorRects()
+void drawColorRects(int passedPotValue, int passedEnvelopeValue)
 { 
-  int rectColor = baseColor;
+  int colorGradient = 40;
+  int audioGradientMax = 500;
+  
+  int audioModifier = map(passedEnvelopeValue,6,MAX_ENVELOPE,0,audioGradientMax);
+  int rectColor = map(passedPotValue,0,1023,0,1536);
+  
   for (int c = 0; c < 16; c++)
-      {
+    {
       matrix.drawRect(c, c, 32-2*c, 32-2*c, matrix.ColorHSV(rectColor + audioModifier, 255, 255, true));
       rectColor = rectColor + colorGradient;
-      }
+    }
   matrix.swapBuffers(true);
 }
 
-void circularDivision()
+void circularDivision(int passedPotValue, int passedEnvelopeValue)
 {
+  int powerOf2[]={0,1,2,4,8,16};
+  int divisions = map(passedEnvelopeValue, 6, MAX_ENVELOPE, 0, 2);
   
+  int radius = round(powerOf2[5-divisions])/2;
+  int rows_cols = round(powerOf2[5-divisions])*2;
+
+  static int colorRandomizer;
+  
+  uint16_t color_palette1[] =
+    {
+       matrix.Color333(7,0,0), // red
+       matrix.Color333(0,7,0), // green
+       matrix.Color333(0,0,7), // blue
+       matrix.Color333(7,7,0), // yellow
+       matrix.Color333(0,7,7), // cyan
+       matrix.Color333(7,0,7) // purple
+      
+    };
+
+  uint16_t color_palette2[] =
+    {
+       matrix.Color333(7,0,2), // magenta
+       matrix.Color333(5,0,3), // pink
+       matrix.Color333(3,0,7), // violet
+       matrix.Color333(0,4,7), // aqua
+       matrix.Color333(0,7,4), // azure
+       matrix.Color333(2,4,2) // moss   
+      
+    };
+
+  uint16_t color_palette3[] =
+    {
+       matrix.Color333(1,1,1), // white
+       matrix.Color333(1,0,0), // dim red
+       matrix.Color333(0,0,0), // black
+       matrix.Color333(7,7,7), // bright white
+       matrix.Color333(3,3,3), // medium white
+       matrix.Color333(0,0,0) // black   
+      
+    };
+
+  uint16_t color_palette4[] =
+    {
+       matrix.Color333(7,0,7), // purple
+       matrix.Color333(7,7,0), // yellow
+       matrix.Color333(7,4,7), // orange
+       matrix.Color333(0,0,0), // black
+       matrix.Color333(0,7,4), // azure
+       matrix.Color333(7,0,2)  // magenta
+      
+    };
+  
+  if (divisions >= 1)
+    colorRandomizer = random(0,4);
+    
+
+  for (int a = 0; a < rows_cols; a++)
+      {
+        for (int b = 0; b < rows_cols; b++)
+          {
+            static int x, y;
+            x = (b * radius*2)-radius;
+            y = (a * radius*2)-radius;
+            if (colorRandomizer%6==0)
+              matrix.fillCircle(x,y,radius,color_palette1[(a+b)%2+colorRandomizer]);
+            else if (colorRandomizer%6==1)
+              matrix.fillCircle(x,y,radius,color_palette2[(a+b)%2+colorRandomizer]);
+            else if (colorRandomizer%6==2)
+              matrix.fillCircle(x,y,radius,color_palette3[(a+b)%2+colorRandomizer]);     
+            else
+              matrix.fillCircle(x,y,radius,color_palette4[(a+b)%2+colorRandomizer]);      
+          }
+      }
+  matrix.swapBuffers(true);
 }
 
 void redBlueFade()
